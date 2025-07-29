b2cfce9b6a9fd1859883ec54cf1eb9506de99d38
wyj <cayww9@gmail.com>
2025-07-29 13:56:19 +0800

    1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute top-8 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+        zhouguoxia072777@icloud.com
       </div>
     </div>
   </div>
